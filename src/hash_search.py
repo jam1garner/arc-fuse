f9ccf2f7e31bf91c0e6900a64043605f8a4b2852
jam1garner <jam1.mcleod@hotmail.com>
2019-05-16 12:44:55 -0400

    Add hash reverse lookup script

---
+++ src/hash_search.py
@@ -0,0 +1,14 @@
+from sys import argv
+from zlib import crc32
+
+crcs = {}
+with open('hashes.txt', 'r') as f:
+    for i in f.readlines():
+        text = i.rstrip()
+        crcs[crc32(text.encode('ascii')) & 0xFFFFFFFF] = text
+
+while True:
+    try:
+        print(crcs[int(input("hash: "), 16)])
+    except Exception(e):
+        print(e)
